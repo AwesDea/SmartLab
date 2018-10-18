59e37c154e383c12df8085b467172796655e7213
awesdea <maisaai1997@gmail.com>
2018-10-18 18:39:56 +0330

    esp fan bug fixes

--- Arduino/esp_fan/esp_fan.ino
+++ Arduino/esp_fan/esp_fan.ino
@@ -19,8 +19,8 @@
 
 //
 /************ Global State ******************/
-#define IR            2              // Pin connected to the IR. GPIO 0
-#define FAN_SPEED     0              // Pin connected to the Fan and sets speed of the fan. GPIO 2 
+#define IR            2              // Pin connected to the IR. GPIO 2
+#define FAN_SPEED     0              // Pin connected to the Fan and sets speed of the fan. GPIO 0
 
 
 
@@ -45,8 +45,8 @@ void set_fan_state(int state);
 
 void MQTT_connect();
 
+//IR setup
 IRrecv irrecv(IR);
-
 decode_results results;
 
 
@@ -66,9 +66,12 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
+  
   Serial.println();
   Serial.println("WiFi connected");
   Serial.println("IP address: "); Serial.println(WiFi.localIP());
+  pi_notif("ESP1 WIFI connected.")
+  
   // Setup MQTT subscription for feeds.
   mqtt.subscribe(&esp_fan);
 }
@@ -76,10 +79,8 @@ void setup() {
 void loop() {
 
   //********************  TODO set mqtt feedback for rpi  ***************************
-
-
-  // Ensure the connection to the MQTT server is alive (this will make the first
-  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  /* Ensure the connection to the MQTT server is alive (this will make the first
+   connection and automatically reconnect when disconnected).  See the MQTT_connect*/
   MQTT_connect();
   // this is our 'wait for incoming subscription packets' busy subloop
 
@@ -88,6 +89,7 @@ void loop() {
   if (irrecv.decode(&results)) {
     unsigned int value = results.value;
     pi_ir.publish(value);//send ir received value to rpi
+    pi_notif.publish(value);//send ir received value to rpi
     irrecv.resume(); // Receive the next value
   }
 
@@ -99,19 +101,23 @@ void loop() {
       char *message = (char *)esp_fan.lastread;
       Serial.print(F("Got: "));
       Serial.println(message);
+      
       // Check if the message was off, low, medium or high.
       if (strncmp(message, "off", 3) == 0) {
         // Turn the fan off
         set_fan_state(0);
       }
+      
       else if (strncmp(message, "low", 3) == 0) {
         // Turn the fan to low speed
         set_fan_state(1);
       }
+      
       else if (strncmp(message, "medium", 6) == 0) {
         // Turn the fan to medium speed
         set_fan_state(2);
       }
+      
       else if (strncmp(message, "high", 4) == 0) {
         // Turn the fan to high speed
         set_fan_state(3);
@@ -142,7 +148,7 @@ void MQTT_connect() {
       while (1);
     }
   }
-  Serial.println("MQTT Connected!");
+   pi_notif("ESP1 MQTT connected.")
 }
 //function for setting speed of fan
 void set_fan_state(int state) {
@@ -151,23 +157,23 @@ void set_fan_state(int state) {
     Serial.println(state);
     pi_notif.publish("Fan turned OFF");
   }
+  
   else if (state == 1) {
     analogWrite(FAN_SPEED, 130); // Send PWM signal to L298N FAN_SPEED pin
     Serial.println(state);
     pi_notif.publish("Fan turned LOW");
-
   }
+  
   else if (state == 2) {
     analogWrite(FAN_SPEED, 185); // Send PWM signal to L298N FAN_SPEED pin
     Serial.println(state);
     pi_notif.publish("Fan turned MEDIUM");
-
   }
+  
   else if (state == 3) {
     analogWrite(FAN_SPEED, 255); // Send PWM signal to L298N FAN_SPEED pin
     Serial.println(state);
     pi_notif.publish("Fan turned HIGH");
-
   }
 }
 
